396baafb70036ab4b433a18a49b35a826d3a55be
Scott Taylor <rscott.taylor01@gmail.com>
2023-05-05 08:29:08 -0500

    Completed Lab

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction (callBack) {
+    callBack();
+}
+
+function returnsANamedFunction () {
+    return function namedFun() {
+
+    }
+}
+
+function returnsAnAnonymousFunction () {
+    return function () {
+
+    }
+}
+// P.S. LOVE the cat <3
